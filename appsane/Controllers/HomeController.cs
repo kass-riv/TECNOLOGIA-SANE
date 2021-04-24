1d82d45aff40fa37f43ea7aeffc0635c245f1c57
kass_riv <kasandra.riveral@gmail.com>
2021-04-24 02:04:10 -0500

    nosotrosp

--- appsane/Controllers/HomeController.cs
+++ appsane/Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace appsane.Controllers
         {
             return View();
         }
+    
+        public IActionResult nosotros()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
