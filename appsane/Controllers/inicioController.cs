4f3ddd4b5fd4c7ce4d36d07ab4c767275883f0f3
Neyser Tacanga <tacangan@gmail.com>
2021-05-07 18:59:01 -0500

    Creando Ingreso

---
+++ appsane/Controllers/inicioController.cs
@@ -0,0 +1,15 @@
+
+using Microsoft.AspNetCore.Mvc;
+namespace appsane
+{
+    
+     public class inicioController : Controller
+     {
+         public IActionResult inicio(){
+              return View();
+         }
+
+
+     }
+        
+}
